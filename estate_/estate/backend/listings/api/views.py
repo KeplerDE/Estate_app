4919c01adfbd7ffa916ef0ce87bb8577c0392dd9
keple <keplerde@gmail.com>
2023-07-13 19:18:21 +0200

    make request form submit property

--- estate_/estate/backend/listings/api/views.py
+++ estate_/estate/backend/listings/api/views.py
@@ -1,7 +1,13 @@
-from .serializers import ListingSerializer
 from listings.models import Listing
 from rest_framework import generics
 
+# Import the ListingSerializer class
+from .serializers import ListingSerializer
+
 class ListingList(generics.ListAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+class ListingCreate(generics.CreateAPIView):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
